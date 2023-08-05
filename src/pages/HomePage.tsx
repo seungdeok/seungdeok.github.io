ce778f0e776d7592398bea1dea614793cc227de5
seungdeok <akk9132@naver.com>
2023-08-05 16:21:51 +0900

    feat: Add card component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { Box, Link, Typography, Container } from '@mui/material';
 import { SDAppBar } from '../components/AppBar';
+import { SDCard } from '../components/Card';
+import { cardItems } from '../configs/card';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://seungdeok.github.io/">
+        seungdeok.github.io
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
@@ -21,9 +23,22 @@ export const HomePage = () => {
       <Container maxWidth="lg">
         <Box component="main">
           <Typography variant="h4" component="h1" gutterBottom>
-            Material UI Create React App example in TypeScript
+            Make, Share, Write
           </Typography>
-          <Copyright />
+          {cardItems.map((item) => {
+            return (
+              <SDCard
+                key={item.label}
+                label={item.label}
+                description={item.description}
+                githubURL={item.githubURL}
+                webURL={item.webURL}
+              />
+            );
+          })}
+          <Box marginTop={4}>
+            <Copyright />
+          </Box>
         </Box>
       </Container>
     </Box>
